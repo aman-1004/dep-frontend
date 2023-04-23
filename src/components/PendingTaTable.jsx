2e10b18d141a26ce0f6350b366173c65424e6ced
Lakshay-Bansal08 <blakshay801@gmail.com>
2023-04-23 17:57:39 +0530

    changes on pending application site

--- src/components/PendingTaTable.jsx
+++ src/components/PendingTaTable.jsx
@@ -22,29 +22,29 @@ let stageName = [
 function PendingTaTable(props) {
   console.log(props.data);
   return (
-    <div>
-      <table>
-        <thead>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-screen-xl mx-auto">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="2xl:text-lg text-gray-700 uppercase bg-gray-50">
           <tr>
-            <th>Application Id</th>
-            <th>User Email</th>
-            <th>Name</th>
-            <th>Created On</th>
-            <th>Status</th>
-            <th>Form</th>
+            <th className="2xl:text-lg text-gray-700 uppercase bg-gray-50">Application Id</th>
+            <th scope="col" className="px-6 py-3">User Email</th>
+            <th scope="col" className="px-6 py-3">Name</th>
+            <th scope="col" className="px-6 py-3">Created On</th>
+            <th scope="col" className="px-6 py-3">Status</th>
+            <th scope="col" className="px-6 py-3">Form</th>
           </tr>
         </thead>
         {props.data.map((item) => {
           return (
             <tbody key={Math.random()}>
-              <tr>
-                <th>{item.id}</th>
-                <td>{item.user.emailId}</td>
-                <td>{item.user.firstName + " " + item.user.lastName}</td>
-                <td>{item.fillDate}</td>
-                <td>{stageName[item.stageCurrent]}</td>
-                <td>
-                  <Link to={"/establish/viewTa/"} />
+              <tr className="bg-white border-b">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{item.id}</th>
+                <td className="px-6 py-4">{item.user.emailId}</td>
+                <td className="px-6 py-4">{item.user.firstName + " " + item.user.lastName}</td>
+                <td className="px-6 py-4">{item.fillDate}</td>
+                <td className="px-6 py-4">{stageName[item.stageCurrent]}</td>
+                <td className="px-6 py-4">
+                  <Link to={"/establish/viewTa/"} className="text-blue-500 font-semibold hover:underline">View Application </Link>
                 </td>
                 {/* <td >
               <Modal>
