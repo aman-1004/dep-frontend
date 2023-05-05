4e232e758878f1cad5eca559b69ca181b68800b3
aman-1004 <2020ceb1004@iitrpr.ac.in>
2023-05-06 01:16:59 +0530

    profile page shows signature

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef} from 'react'
 import { LoginContext } from '../LoginContext'
 import { stringify } from 'postcss'
 import Form from '../components/Form'
@@ -9,16 +9,26 @@ import Input from '../components/Input'
 function formatDate(date) {
     return new Date(date).toISOString().substring(0, 10)
 }
+
 export default function UserProfile() {
     const [user, setUser] = useContext(LoginContext) 
-    useEffect(()=>console.log('user is', user), [])
+    const imgRef = useRef(null)
+    useEffect(()=>{
+      fetch('/api/getSignImage', {method:"POST"}).then(res => res.blob()).then(blob => {
+        const url = URL.createObjectURL(blob);
+        imgRef.current.src = url
+      })
+      console.log('user is', user)
+    }, [])
   return (
     // <div>{JSON.stringify(user)}</div>
+
   <Form>
     <Input label={"First Name"} name="First Name" type="text" value={user.firstName} readOnly />
     <Input label={"Last Name"} name="Last Name" type="text" value={user.lastName} readOnly />
     {/* <Input label={"Designation"} name="Designation" type="text" value={user.designation} readOnly /> */}
     <Input label={"Date Of Joining"} name="Date of Joining" type="date" value={formatDate(user.dateOfJoining)} readOnly />
+    <img ref={imgRef} placeholder='signature.jpg'></img>
   </Form>
   )
 }
